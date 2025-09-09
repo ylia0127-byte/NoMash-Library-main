72250d44175489dce667a41d3b5a1927b33cd76e
yicheng <ylia0127@student.monash.edu>
2025-09-09 19:57:25 +1000

    vuejs set up

--- src/main.js
+++ src/main.js
@@ -8,8 +8,21 @@ import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
 
 
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDL9ahXqppxbLI_TSy0viG16FWyaXrMN9g",
+  authDomain: "week7-yicheng.firebaseapp.com",
+  projectId: "week7-yicheng",
+  storageBucket: "week7-yicheng.firebasestorage.app",
+  messagingSenderId: "388463051321",
+  appId: "1:388463051321:web:df95e9054e119a584803e8",
+  measurementId: "G-DBGSHBBV4L"
+}
+
+initializeApp(firebaseConfig)
+
 const app = createApp(App)
 app.use(PrimeVue, { theme: { preset: Aura } })
 app.use(router)
-
 app.mount('#app')
